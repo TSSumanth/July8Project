05dea2e78a0091c11c8d2add676ff780689b9068
Sumanth t <tssumanth18@gmail.com>
2022-08-18 11:21:41 +0530

    run from command line

--- src/FunctionalLibrary/HomepageFunctions.java
+++ src/FunctionalLibrary/HomepageFunctions.java
@@ -10,9 +10,30 @@ public class HomepageFunctions extends GenericComponents
 {
 	Properties locators = GenericComponents.getObjectLibrary("HomePageObjects");
 	GenericComponents gc = new GenericComponents();
+	
 	public void clickOnCheckboxesLink()
 	{
 		gc.click(By.xpath((String)locators.get("checkboxs_xpath")));
+	}
+	
+	public boolean verifyCheckbox1IsSelected()
+	{
+		return gc.verifyCheckboxIsSelected(By.xpath((String)locators.get("checkbox1_xpath")));
+	}
+	
+	public void clickOnCheckbox1()
+	{
+		gc.click(By.xpath((String)locators.get("checkbox1_xpath")));
+	}
+	
+	public void clickOnCheckbox2()
+	{
+		gc.click(By.xpath((String)locators.get("checkbox2_xpath")));
+	}
+	
+	public boolean verifyCheckbox2IsSelected()
+	{
+		return gc.verifyCheckboxIsSelected(By.xpath((String)locators.get("checkbox2_xpath")));
 	}
 
 }
--- tests/homePage/Homepagetest.java
+++ tests/homePage/Homepagetest.java
@@ -1,13 +1,10 @@
 package homePage;
 
 import static org.testng.Assert.assertEquals;
-
-import org.openqa.selenium.By;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 import FunctionalLibrary.HomepageFunctions;
 import GenericComponents.GenericComponents;
 
@@ -34,21 +31,19 @@ public class Homepagetest extends GenericComponents
 	@Test
 	public void validateCheckbox1()
 	{
-		obj.click(By.xpath("//a[text()='Checkboxes' and @href='/checkboxes']"));
-		obj.sleep(5);
-		assertEquals(obj.verifyCheckboxIsSelected(By.xpath("//input[@type='checkbox'][1]")), false);
-		obj.click(By.xpath("//input[@type='checkbox'][1]"));
-		assertEquals(obj.verifyCheckboxIsSelected(By.xpath("//input[@type='checkbox'][1]")), true);
+		hpf.clickOnCheckboxesLink();
+		assertEquals(hpf.verifyCheckbox1IsSelected(), false);
+		hpf.clickOnCheckbox1();
+		assertEquals(hpf.verifyCheckbox1IsSelected(), true);
 	}
 	
 	@Test
 	public void validateCheckbox2()
 	{
-		obj.click(By.xpath("//a[text()='Checkboxes' and @href='/checkboxes']"));
-		obj.sleep(5);
-		assertEquals(obj.verifyCheckboxIsSelected(By.xpath("//input[@type='checkbox'][2]")), true);
-		obj.click(By.xpath("//input[@type='checkbox'][2]"));
-		assertEquals(obj.verifyCheckboxIsSelected(By.xpath("//input[@type='checkbox'][2]")), false);
+		hpf.clickOnCheckboxesLink();
+		assertEquals(hpf.verifyCheckbox2IsSelected(), true);
+		hpf.clickOnCheckbox2();
+		assertEquals(hpf.verifyCheckbox2IsSelected(), false);
 	}
 	
 	@AfterMethod
